1c73cb02880aff373cc7185a9fc82cf1fcc898ff
Alok kannaujia <alokabc25@gmail.com>
2024-11-04 00:24:37 +0530

    update

---
+++ app/api/check/route.ts
@@ -0,0 +1,47 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const feedback = await req.json();
+
+    // Validate feedback data using zod
+    // const validation = feedbackSchema.safeParse(feedback);
+
+    // console.log("validation", validation);
+
+    if (!feedback) {
+      return NextResponse.json(
+        { success: false, error: "no feedback data" },
+        { status: 400 }
+      );
+    }
+
+    // Save feedback to database
+    const submitFeedback = await prisma.feedback.create({
+      data: {
+        name: feedback?.name,
+        email: feedback?.email,
+        feedback: feedback?.feedback,
+        rating: feedback?.rating,
+        projectId: feedback?.projectId,
+      },
+    });
+
+    // console.log("submitFeedback: ", submitFeedback);
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Feedback received successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { success: false, error: "Failed to process feedback" },
+      { status: 500 }
+    );
+  }
+}
